04deb74c2a07defa601b089f8d74000201fc0cdf
shivam <141029609+js-1608@users.noreply.github.com>
2024-07-01 18:04:52 +0530

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,40 @@
 import React from 'react';
-  import Carousel from '../components/Carousel';
-  const About = () =>  {
+import banner from "../assests/about_banner.png";
+import Banner from '../components/Banner';
+const About = () => {
 	return (
-	  <div>
-		This is a about page
-		<Carousel/>
-	  </div>
+		<div>
+			<Banner
+				src={banner}
+				title="About Us"
+				text="Welcome to Ultrapure Envirocare Pvt Ltd, a leading provider of innovative air purification solutions dedicated to enhancing indoor air quality worldwide."
+				updated=""
+				buttonUrl=""
+				buttonText=""
+			/>
+
+			<div className="container mx-auto p-4">
+				<div className="flex flex-col md:flex-row">
+				<div className="w-full md:w-1/2 p-4  ">
+						{/* <img src="" alt="Email Logo" className="w-3/5 h-4/5  m-auto header_container" /> */}
+						<div className="w-3/5  p-4 m-auto header_container">
+							<h5 className='font-bold'>Head Office</h5>
+							3rd Floor, Ion House, Dr. E. Moses Road, Mahalaxmi, Mumbai-400011
+						</div>
+
+					</div>
+					<div className="w-full md:w-1/2 p-4  ">
+						{/* <img src="" alt="Email Logo" className="w-3/5 h-4/5  m-auto header_container" /> */}
+						<div className="w-3/5  p-4 m-auto header_container">
+							<h5 className='font-bold'>Head Office</h5>
+							3rd Floor, Ion House, Dr. E. Moses Road, Mahalaxmi, Mumbai-400011
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
 	);
-  }
-  
-  export default About;
-  
+}
+
+export default About;

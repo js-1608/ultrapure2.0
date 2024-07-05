e661b426b2796cd947d446a34d09ab5523fb0d2f
shivam <141029609+js-1608@users.noreply.github.com>
2024-07-05 14:09:38 +0530

    update

--- src/components/FAQSection.js
+++ src/components/FAQSection.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp ,faChevronRight} from '@fortawesome/free-solid-svg-icons';
 
 const FAQSection = ({ faqs }) => {
   const [openFAQ, setOpenFAQ] = useState(null);
@@ -10,24 +12,23 @@ const FAQSection = ({ faqs }) => {
   return (
     <div>
       {faqs.map(faq => (
-        <div key={faq.id} className="mb-4 p-4 border rounded-lg transition-shadow duration-300 hover:shadow-lg ">
-          <h3
-            onClick={() => handleToggleFAQ(faq.id)}
-            className="text-xl cursor-pointer font-bold mt-2 mb-2"
+        <div key={faq.id} className="mb-4 p-4 border rounded-lg transition-shadow duration-300 hover:shadow-lg">
+          <div 
+            onClick={() => handleToggleFAQ(faq.id)} 
+            className="flex justify-between items-center cursor-pointer"
           >
-            {faq.heading}
-          </h3>
-          {/* <span aria-hidden="true" className='text-end'>&rarr;</span> */}
+            <h3 className="text-xl font-bold mt-2 mb-2">
+              {faq.heading}
+            </h3>
+            <FontAwesomeIcon icon={openFAQ === faq.id ? faChevronDown:  faChevronRight } />
+          </div>
 
           {openFAQ === faq.id && (
-
-            <div className="mt-2 flex flex-wrap place-content-center ">
-
+            <div className="mt-2 flex flex-wrap place-content-center">
               {faq.content.map((item, index) => (
-                <div key={index} className=" m-5 ">
-
+                <div key={index} className="m-5">
                   <img src={item.imageUrl} alt={item.heading} className="w-full h-auto" />
-                  <h4 className=" bg-gray-100 mt-0">
+                  <h4 className="bg-gray-100 mt-0">
                     <a
                       href={item.href}
                       target="_blank"
@@ -35,9 +36,9 @@ const FAQSection = ({ faqs }) => {
                       className="hover:underline block p-2 text-lg font-semibold"
                     >
                       {item.heading}
-                      {/* <span aria-hidden="true" className='text-end'>&rarr;</span> */}
-                    </a> </h4>
-                  <hr className='border-1 border-grey shadow-xl'></hr>
+                    </a>
+                  </h4>
+                  <hr className="border-1 border-grey shadow-xl"></hr>
                 </div>
               ))}
             </div>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,11 +3,19 @@ import React from 'react';
 const ProductList = ({ products }) => {
   return (
     <div className="flex flex-col">
+      <h6 className='text-xl font-bold'>Explore Industries</h6>
+      <hr className='text-black mt-2 border-black' ></hr>
       {products.map(product => (
         <a key={product.id} href={product.href} 
         target="_blank" rel="noopener noreferrer" 
-        className="mb-2 p-4 border rounded-lg transition-shadow duration-300 hover:shadow-lg">
-          {product.name}
+        className="mb-2 p-4  hover:shadow-lg">
+          <h2 className='text-lg font-semibold'>{product.name}</h2>
+              {product.list.map((item, index) => (
+              <div key={index} className=" ">
+                <a>{item}</a>
+            </div>
+          ))}
+
         </a>
       ))}
     </div>

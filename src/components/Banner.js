8ad29ecbd88dc5a4041e4d027e51448c8f4287bd
juhi2116 <141029609+js-1608@users.noreply.github.com>
2024-07-26 07:53:14 +0530

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,8 +9,8 @@ function Banner({ src, title, text, updated, buttonUrl, buttonText }) {
       <img src={src} alt="banner" className="w-full h-full object-cover opacity-75" />
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="p-8 text-center">
-          <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold display_hide">{title}</h1>
-          <p className="mt-6 text-sm md:text-xl lg:text-2xl w-3/4 text-center m-auto">{text}</p>
+          <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold ">{title}</h1>
+          <p className="mt-6 text-sm md:text-xl lg:text-2xl w-3/4 text-center m-auto display_hide">{text}</p>
           <p className="mt-0 text-lg md:text-2xl lg:text-3xl font-bold">{updated}</p>
           {buttonUrl && (
             <a href={buttonUrl} className="sm: mt-1 lg:mt-5 inline-flex items-center px-4 py-2 bg-white text-blue-700 rounded hover:bg-gray-200 font-medium">

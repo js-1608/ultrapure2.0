80b0ffd28cc668a9495d63dd0433166ce47007f8
shivam <141029609+js-1608@users.noreply.github.com>
2024-07-29 17:47:32 +0530

    update

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -8,7 +8,7 @@ const Newsletter = () => {
         <h2 className="text-2xl font-bold mb-2 ">Join Us on Our Mission</h2>
         <p className="text-lg text-justify">Experience the difference that Ultrapure's air purification solutions can make in your indoor environment. Together, let's create a healthier and cleaner world for future generations.</p>
       </div>
-      <div className="m-4 lg:w-2/5 sm:w-full text-end">
+      <div className="m-4 lg:w-2/5  text-end">
         <button className="bg-white text-[#90C03E] font-bold py-2 px-10 rounded hover:bg-gray-200  ">
           Contact Us  <span aria-hidden="true">&rarr;</span>
         </button>

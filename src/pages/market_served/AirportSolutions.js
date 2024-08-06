096d21dfdeb2f860cfd7830da165368dbd6179cb
shivam <141029609+js-1608@users.noreply.github.com>
2024-08-06 14:39:32 +0530

    UPDATED

--- src/pages/market_served/AirportSolutions.js
+++ src/pages/market_served/AirportSolutions.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from "react";
 
-export const AirportSolutions = ({ imageUrl, problem, tableData ,health}) => {
+export const AirportSolutions = ({ imageUrl, problem, tableData, health }) => {
   return (
-    <div className="p-6 border rounded shadow-lg">
-        <div className='flex'>
-      <img src={imageUrl} alt="Airport" className="w-full h-64 object-cover  rounded m-auto display_hide" />
-      <div className='p-2'>
-      <img src={imageUrl} alt="Airport" className="w-full h-64 object-cover  rounded m-auto display_hide_big" />
+    <div className=" p-0 lg:p-6">
+      <div className="flex">
+        <div className="p-2 w-full lg:w-3/4 sm:w-full">
+          <img
+            src={imageUrl}
+            alt="Airport"
+            className="w-full h-64 object-cover  rounded m-auto display_hide_big"
+          />
 
-        <h4 className='text-xl font-semibold'>Problem:</h4>
-      <p className="mb-4 text-lg text-justify">{problem}</p>
-      <h4 className='text-xl font-semibold'>Health:</h4>
-      <p className="mb-4 text-lg text-justify">{health}</p>
-      </div>
+          <h4 className="text-xl font-semibold">The Problem:</h4>
+          <p className="mb-4 text-md text-justify">{problem}</p>
+          <h4 className="text-xl font-semibold">Health Risks:</h4>
+          <ul className="mb-4 text-md list-disc pl-5">
+            {health.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+        {/* <img
+          src={imageUrl}
+          alt="Airport"
+          className="w-full h-64 object-cover  rounded m-auto display_hide"
+        /> */}
+ 
       </div>
       <table className="w-full mb-4 border">
         <thead>
@@ -31,8 +44,12 @@ export const AirportSolutions = ({ imageUrl, problem, tableData ,health}) => {
         </tbody>
       </table>
       <div className="flex justify-evenly">
-        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Contact Us</button>
-        <button className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600">Reach Us</button>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+          Contact Us
+        </button>
+        <button className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600">
+          Reach Us
+        </button>
       </div>
     </div>
   );
--- src/pages/market_served/Commerical.js
+++ src/pages/market_served/Commerical.js
@@ -32,7 +32,12 @@ export default function Commerical() {
 
   const imageUrl = "publicbuilding/public (1).png";
   const problem = " Airports experience high foot traffic, leading to elevated levels of airborne pathogens, dust, VOCs, and pollutants from aircraft and ground vehicles. The constant movement of people and luggage stirs up dust and other particulates, while the presence of travelers from various locations increases the risk of spreading infectious diseases. Health Risks: Respiratory infections, asthma exacerbations, allergic reactions, and transmission of infectious diseases.";
-  const health = "Respiratory infections, asthma exacerbations, allergic reactions, and transmission of infectious diseases."
+  const health = [
+    "Respiratory infections",
+    "Asthma exacerbations",
+    "Allergic reactions",
+    "Transmission of infectious diseases"
+  ];
 
   // halls data
   const HallstableData = [
@@ -46,7 +51,15 @@ export default function Commerical() {
   ];
   const HallsimageUrl = "publicbuilding/public (8).png";
   const Hallsproblem = "Halls and malls are large, enclosed spaces that can accumulate dust, odors, VOCs, and airborne bacteria due to high visitor turnover and diverse activities such as food preparation and retail operations. Poor ventilation can exacerbate these issues, leading to an unpleasant environment for shoppers and staff.";
-  const Hallshealth = "Allergies, respiratory problems, headaches, and increased susceptibility to infections."
+  const Hallshealth =
+    [
+      "Allergies",
+      "Respiratory problems",
+      "Headaches",
+      "Increased susceptibility to infections"
+    ];
+
+
 
   // Restaurants data
   const RestaurantstableData = [
@@ -60,7 +73,12 @@ export default function Commerical() {
   ];
   const RestaurantsimageUrl = "publicbuilding/public (5).png";
   const Restaurantsproblem = "Restaurants generate odors, smoke, VOCs, and airborne grease particles from cooking activities. These pollutants can spread throughout the dining area, creating an unpleasant experience for customers and posing health risks for staff. Proper ventilation and air purification are essential to manage these contaminants.";
-  const Restaurantshealth = "Respiratory issues, allergies, headaches, and irritation from smoke and VOCs";
+  const Restaurantshealth = [
+    "Respiratory issues",
+    "Allergies",
+    "Headaches",
+    "Irritation from smoke and VOCs"
+  ];
 
   // Officies
   const OfficestableData = [
@@ -73,10 +91,15 @@ export default function Commerical() {
   ];
   const OfficesimageUrl = "publicbuilding/public (6).png";
   const Officesproblem = "Office environments often suffer from indoor air pollutants, including VOCs from office equipment, dust, mold spores, and poor ventilation. These factors can lead to 'sick building syndrome,' where occupants experience health issues such as headaches, dizziness, and respiratory problems, impacting productivity and well-being.";
-  const Officeshealth = "Headaches, fatigue, respiratory problems, and irritation of the eyes, nose, and throat.";
-  
-   // Museums & Cultural Institutions
-   const MuseumstableData = [
+  const Officeshealth = [
+    "Headaches",
+    "Fatigue",
+    "Respiratory problems",
+    "Irritation of the eyes, nose, and throat"
+  ];
+
+  // Museums & Cultural Institutions
+  const MuseumstableData = [
     { applicationArea: 'Whole building with Centralized Air Conditioning', solution: 'Pentagon AOP duct air purifiers' },
     { applicationArea: 'Spa areas', solution: 'Virushield/ GermiNOX/ Falcon S/ Falcon C' },
     { applicationArea: 'Washrooms/ restrooms', solution: 'Virushield/ GermiNOX' },
@@ -87,8 +110,41 @@ export default function Commerical() {
   ];
 
   const MuseumsimageUrl = "publicbuilding/public (3).png";
-  const Museumsproblem = "Problems: Airports experience high foot traffic, leading to elevated levels of airborne pathogens, dust, VOCs, and pollutants from aircraft and ground vehicles. The constant movement of people and luggage stirs up dust and other particulates, while the presence of travelers from various locations increases the risk of spreading infectious diseases. Health Risks: Respiratory infections, asthma exacerbations, allergic reactions, and transmission of infectious diseases.";
-  const Museumshealth = "Health Risks: Respiratory infections, asthma exacerbations, allergic reactions, and transmission of infectious diseases."
+  const Museumsproblem = "These institutions face unique challenges in preserving artifacts while maintaining good air quality. Pollutants such as dust, VOCs, mold spores, and humidity can damage valuable collections. Additionally, the high volume of visitors can introduce contaminants that degrade the indoor environment.";
+  const Museumshealth = [
+    "Allergies", "respiratory irritation", "potential long-term effects from exposure to VOCs"
+  ];
+
+  // Airport data
+  const HoteltableData = [
+    { applicationArea: 'Whole building with Centralized Air Conditioning', solution: 'Pentagon AOP duct air purifiers' },
+    { applicationArea: 'Spa areas', solution: 'Virushield/ GermiNOX/ Falcon S/ Falcon C' },
+    { applicationArea: 'Washrooms/ restrooms', solution: 'Virushield/ GermiNOX' },
+    { applicationArea: 'Pantry/ kitchen areas/ Cafeteria/ Lounges', solution: 'Virushield/ GermiNOX' },
+    { applicationArea: 'Waiting areas', solution: 'Virushield/ GermiNOX/ Falcon S/ Falcon C' },
+    { applicationArea: 'Smoking Zones', solution: 'Customized air purification (SmokoNOX)' },
+    { applicationArea: 'Bags Check-in area', solution: 'Virushield/ GermiNOX/ Customized purifier' },
+  ];
+
+  const HotelimageUrl = "publicbuilding/public (1).png";
+  const Hotelproblem = " Hotels must manage odors, allergens, VOCs, and airborne bacteria to ensure guest comfort. Cleaning activities, use of personal care products, and high turnover of guests contribute to poor indoor air quality. Issues such as mold and mildew in humid climates can further degrade air quality, impacting guest satisfaction and health.";
+  const Hotelhealth = ["Respiratory issues", "allergies, asthma", " exposure to mold-related illnesses"
+  ];
+  // school and universities
+  const SchooltableData = [
+    { applicationArea: 'Whole building with Centralized Air Conditioning ', solution: 'Pentagon AOP duct air purifiers' },
+    { applicationArea: 'Teacher Cabins', solution: 'Virushield/ GermiNOX' },
+    { applicationArea: 'Washrooms/ restrooms', solution: 'Virushield/ GermiNOX' },
+    { applicationArea: 'Canteen/ kitchen areas/ Cafeteria ', solution: 'Virushield/ GermiNOX' },
+    { applicationArea: 'Classrooms', solution: 'Virushield/ GermiNOX/ Falcon S/ Falcon C' },
+    { applicationArea: 'Library/ Computer room ', solution: 'Customized air purification, Virushield/ GermiNOX/ Falcon S/ Falcon C' },
+    { applicationArea: 'Laboratories', solution: 'Fresh air unit for gases treatment' },
+  ];
+
+  const SchoolimageUrl = "publicbuilding/public (1).png";
+  const Schoolproblem = " : Educational institutions must address allergens, bacteria, VOCs, and mold spores to protect students' health. Classrooms, laboratories, and common areas can accumulate dust, mold, and chemical fumes, which can affect students' concentration, attendance, and overall well-being.";
+  const Schoolhealth = ["Allergies", " asthma", " respiratory infections", " exposure to harmful chemicals"
+  ];
 
   return (
     <div>
@@ -102,48 +158,64 @@ export default function Commerical() {
       />
       <div className="flex flex-wrap ">
         <div className="flex-1 p-2 w-4/5">
-        {/* Airports */}
-          <section id="Airports">
+          {/* Airports */}
+          <section id="Airports " className=" border rounded shadow-lg m-5">
             <h1 className='text-4xl text-center font-bold '>Airport</h1>
-            <div className="container mx-auto p-4">
+
+            <div className="container mx-auto p-4 ">
               <AirportSolutions imageUrl={imageUrl} problem={problem} tableData={tableData} health={health} />
             </div>
           </section>
 
-          {/* Restaurants */}
-          <section id="Restaurants">
-            <h1 className='text-4xl text-center font-bold '>	Restaurants</h1>
-            <div className="container mx-auto p-4">
-              <AirportSolutions imageUrl={RestaurantsimageUrl} problem={Restaurantsproblem} tableData={RestaurantstableData} health={Restaurantshealth} />
-            </div>
-          </section>
-
-           {/* Officies */}
-           <section id="Offices">
-            <h1 className='text-4xl text-center font-bold '>Offices</h1>
-            <div className="container mx-auto p-4">
-              <AirportSolutions imageUrl={OfficesimageUrl} problem={Officesproblem} tableData={OfficestableData} health={Officeshealth} />
-            </div>
-          </section>
-
-             {/* Halls */}
-             <section id="Halls">
-            <h1 className='text-4xl text-center font-bold '>	Halls & Malls</h1>
-            <div className="container mx-auto p-4">
-              <AirportSolutions imageUrl={HallsimageUrl} problem={Hallsproblem} tableData={HallstableData} health={Hallshealth} />
-            </div>
-          </section>
-
-                 {/* Musuems */}
-                 <section id="Museums">
+          {/* Musuems */}
+          <section id="Museums" className=" border rounded shadow-lg m-5">
             <h1 className='text-4xl text-center font-bold '>Museums</h1>
             <div className="container mx-auto p-4">
               <AirportSolutions imageUrl={MuseumsimageUrl} problem={Museumsproblem} tableData={MuseumstableData} health={Museumshealth} />
             </div>
           </section>
 
-        </div>
+          {/* Schools */}
+          <section id="Schools" className=" border rounded shadow-lg m-5">
+            <h1 className='text-4xl text-center font-bold '>Schools & Universities</h1>
+            <div className="container mx-auto p-4 ">
+              <AirportSolutions imageUrl={SchoolimageUrl} problem={Schoolproblem} tableData={SchooltableData} health={Schoolhealth} />
+            </div>
+          </section>
 
+          {/* Restaurants */}
+          <section id="Restaurants" className=" border rounded shadow-lg m-5">
+            <h1 className='text-4xl text-center font-bold '>	Restaurants</h1>
+            <div className="container mx-auto p-4">
+              <AirportSolutions imageUrl={RestaurantsimageUrl} problem={Restaurantsproblem} tableData={RestaurantstableData} health={Restaurantshealth} />
+            </div>
+          </section>
+
+          {/* Officies */}
+          <section id="Offices" className=" border rounded shadow-lg m-5">
+            <h1 className='text-4xl text-center font-bold '>Offices</h1>
+            <div className="container mx-auto p-4">
+              <AirportSolutions imageUrl={OfficesimageUrl} problem={Officesproblem} tableData={OfficestableData} health={Officeshealth} />
+            </div>
+          </section>
+
+          {/* Hotels */}
+          <section id="Hotels" className=" border rounded shadow-lg m-5">
+            <h1 className='text-4xl text-center font-bold '>Hotels</h1>
+            <div className="container mx-auto p-4 ">
+              <AirportSolutions imageUrl={HotelimageUrl} problem={Hotelproblem} tableData={HoteltableData} health={Hotelhealth} />
+            </div>
+          </section>
+
+          {/* Halls */}
+          <section id="Halls" className=" border rounded shadow-lg m-5">
+            <h1 className='text-4xl text-center font-bold '>	Halls & Malls</h1>
+            <div className="container mx-auto p-4">
+              <AirportSolutions imageUrl={HallsimageUrl} problem={Hallsproblem} tableData={HallstableData} health={Hallshealth} />
+            </div>
+          </section>
+        </div>
+        
         <div className=" p-4 w-1/5  border border-black m-5 rounded display_hide">
           <ProductList faqs={faqs} />
         </div>
